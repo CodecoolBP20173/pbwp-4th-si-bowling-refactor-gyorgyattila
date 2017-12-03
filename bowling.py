5a7883fe3469d70f9acf3dbfea228a8263569d2e
Meszaros Gyorgy Attila <sirex1993@gmail.com>
2017-12-03 20:52:32 +0100

    refactor the score function

--- bowling.py
+++ bowling.py
@@ -1,37 +1,27 @@
 def score(game):
-    #(score("5/3-----------------")
     result = 0
     frame = 1
-    in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
-        else:
+        if game[i] != '/':
             result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 19  and get_value(game[i]) == 10:
             if game[i] == '/':
+                result += 10 - get_value(game[i-1])
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            else:
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
-        last = get_value(game[i])
-        if in_first_half == False:
-            frame += 1
-            in_first_half = True
+        if game[i].lower()=='x':
+            frame += 2
         else:
-            in_first_half = False
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
             frame += 1
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in "123456789":
         return int(char)
     elif char == 'X' or char == 'x' or char =='/':
         return 10
@@ -39,9 +29,3 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
-
-"""
-def round():
-    for step in game: 
-        if step 
-"""
